dfea89a71993b1314cea8ce20bb8a87048a0197a
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-29 22:53:09 +0200

    Aggiunto la copia della lista e la posizione del cognome

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,11 @@ var cognome = prompt("Inserisci Cognome")
 
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var iscritti = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
-iscritti.push(cognome);
 //3. stampa la lista ordinata alfabeticamente
-document.getElementById("lista_ordinata").innerHTML = iscritti.sort();
+iscritti.push(cognome);
+var iscrittiCopia = iscritti.slice();
+var iscrittiSort = iscrittiCopia.sort()
+document.getElementById("lista_ordinata").innerHTML = iscrittiSort;
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var posizione = iscrittiSort.indexOf(cognome) + 1;
+console.log(posizione);
