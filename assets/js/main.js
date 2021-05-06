3ededb8a00aa76ee23e3be83c208971a79b01411
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-06 15:56:05 +0200

    lista con cognomi con iniziale maiuscola

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,9 @@ var cognome = prompt("Inserisci Cognome")
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var iscritti = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 //3. stampa la lista ordinata alfabeticamente
-iscritti.push(cognome);
+iscritti.push(cognome.charAt(0).toUpperCase() + cognome.slice(1));
 var iscrittiCopia = iscritti.slice();
+
 var iscrittiSort = iscrittiCopia.sort()
 document.getElementById("lista_ordinata").innerHTML = iscrittiSort;
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
